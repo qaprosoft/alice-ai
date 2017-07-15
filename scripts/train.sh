99890ee2f1da50d291efa617bab7cf5434776ad8
KirillBugrim <kbugrim@qaprosoft.com>
2017-07-15 20:39:27 +0000

    start using latest neural network state for next training process

--- scripts/train.sh
+++ scripts/train.sh
@@ -9,4 +9,4 @@ echo IMAGE_HOME: $IMAGE_HOME
 export ANNOTATION_HOME=$HOME/tools/alice/data/ideal/ann
 echo ANNOTATION_HOME: $ANNOTATION_HOME
 
-nohup $DARKFLOW_HOME/flow --train --annotation $ANNOTATION_HOME --dataset $IMAGE_HOME --model $DARKFLOW_HOME/cfg/tiny-ai-weights-lc-mlb.cfg --load 344502 --trainer adam --gpu 0.9 --lr 1e-5 --keep 15 --backup $DARKFLOW_HOME/ckpt/mlb_branch/ --batch 8 --save 2900 --epoch 3000 > ../logs/train.log &
+nohup $DARKFLOW_HOME/flow --train --annotation $ANNOTATION_HOME --dataset $IMAGE_HOME --model $DARKFLOW_HOME/cfg/tiny-ai-weights-lc-mlb.cfg --load -1 --trainer adam --gpu 0.9 --lr 1e-5 --keep 15 --backup $DARKFLOW_HOME/ckpt/mlb_branch/ --batch 8 --save 2900 --epoch 3000 > ../logs/train.log &
