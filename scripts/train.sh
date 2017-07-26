1240fb02eaceb60ca464d26f7c783293c551efda
KirillBugrim <kbugrim@qaprosoft.com>
2017-07-26 11:50:58 +0000

    refactored to use dynamic list of img/ann and symlinks

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
+#export DATA_PATTERN=$1
+##echo DATA_PATTERN=$DATA_PATTERN
+#if [ "$DATA_PATTERN" = "" ]; then
+#	export DATA_PATTERN="*"
+#fi
+#echo DATA_PATTERN=$DATA_PATTERN
+
 #TODO: move general properties declaration in to environment as prerequisites
-export DARKFLOW_HOME=/home/ubuntu/darkflow/
+export DARKFLOW_HOME=/home/ubuntu/darkflow
 
-export IMAGE_HOME=$HOME/tools/alice/data/ideal/img
-echo IMAGE_HOME: $IMAGE_HOME
+export DATA_HOME=$HOME/tools/alice/data/2.0
+echo DATA_HOME: $DATA_HOME
 
-export ANNOTATION_HOME=$HOME/tools/alice/data/ideal/ann
-echo ANNOTATION_HOME: $ANNOTATION_HOME
+#make temporary folder with symlinks onto the train set data (img and ann)
+export TMP_DATA=/tmp/train
 
-nohup $DARKFLOW_HOME/flow --train --annotation $ANNOTATION_HOME --dataset $IMAGE_HOME --model $DARKFLOW_HOME/cfg/tiny-ai-weights-lc-mlb.cfg --load -1 --trainer adam --gpu 0.9 --lr 1e-5 --keep 15 --backup $DARKFLOW_HOME/ckpt/mlb_branch/ --batch 8 --save 2900 --epoch 3000 > ../logs/train.log &
+#clean existing $TMP_DATA folder if any
+if [ -d $TMP_DATA ]; then
+        echo Removing $TMP_DATA...
+        rm -rf $TMP_DATA
+	echo creating clean $TMP_DATA folder
+fi
+
+
+mkdir $TMP_DATA
+#recursively create symlinks from $DATA_HOME to $TMP_DATA
+mkdir $TMP_DATA/img
+mkdir $TMP_DATA/ann
+
+find $DATA_HOME -name '*.png' -exec ln -vs "{}" $TMP_DATA/img/ ';'
+find $DATA_HOME -name '*.xml' -exec ln -vs "{}" $TMP_DATA/ann/ ';'
+
+#nohup $DARKFLOW_HOME/flow --train --annotation $ANNOTATION_HOME --dataset $IMAGE_HOME --model $DARKFLOW_HOME/cfg/ai-2.0.cfg --load $DARKFLOW_HOME/ckpt/2.0/ai-2.0.weights --trainer adam --gpu 0.9 --lr 1e-3 --keep 15 --backup $DARKFLOW_HOME/ckpt/2.0/ --batch 8 --save 2900 --epoch 3000 > ../logs/train.log &
+
+nohup $DARKFLOW_HOME/flow --train --annotation $TMP_DATA/ann --dataset $TMP_DATA/img --model $DARKFLOW_HOME/cfg/ai-2.0.cfg  --load -1 --trainer adam --gpu 0.9 --lr 1e-5 --keep 15 --backup $DARKFLOW_HOME/ckpt/2.0/ --batch 8 --save 2900 --epoch 3000 > ../logs/train.log &
